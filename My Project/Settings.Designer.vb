f9f49f5a52ee3cd1365f972694f4497227e96bc8
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-12-28 01:41:02 +0800

    Initial commit

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -57,8 +57,8 @@ Namespace My
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("server=localhost;user id=root;persistsecurityinfo=False;database=inventory;passwo"& _ 
-            "rd=ferdinand")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("persistsecurityinfo=False;server=localhost;user id=root;password=123456;database="& _ 
+            "inventory")>  _
         Public ReadOnly Property inventoryConnectionString() As String
             Get
                 Return CType(Me("inventoryConnectionString"),String)
